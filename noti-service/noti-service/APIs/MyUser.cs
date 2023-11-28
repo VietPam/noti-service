cd451f13c5f9c5174d9b1ea9f6930abe159ed49e
vietpham-stvg <82923647+phamquangviet891@users.noreply.github.com>
2023-11-28 14:13:18 +0700

    update
    done createUserAsync(code)

--- noti-service/noti-service/APIs/MyUser.cs
+++ noti-service/noti-service/APIs/MyUser.cs
@@ -1,4 +1,6 @@
-﻿namespace noti_service.APIs
+﻿using noti_service.Model;
+
+namespace noti_service.APIs
 {
     public class MyUser
     {
@@ -9,7 +11,27 @@
         }
         public async Task<bool> createUserAsync(string code)
         {
-            return false;
+            if (string.IsNullOrEmpty(code))
+            {
+                return false;
+            }
+            using(DataContext context = new DataContext())
+            {
+                SqlUser? existing_user= context.users.Where(s=>s.code == code).FirstOrDefault();
+                if (existing_user != null)
+                {
+                    return false;
+                }
+                else
+                {
+                    SqlUser newUser= new SqlUser();
+                    newUser.ID = DateTime.Now.Ticks;
+                    newUser.code = code;
+                    context.users.Add(newUser);
+                    await context.SaveChangesAsync();
+                    return true;
+                }
+            }
         }
 
         public List<UserDTOResponse> getListUser()
